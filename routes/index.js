000a03b589bbc701592815e1a2c9a306dd200e7f
Chuck <dota2mm@163.com>
2016-12-30 16:52:23 +0800

    默认配置、路由

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+module.export = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts')
+  })
+  app.use('/signup', require('./signup'))
+  app.use('/signin', require('./signin'))
+  app.use('/signout', require('./signout'))
+  app.use('/posts', require('./posts'))
+}
