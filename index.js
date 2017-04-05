0dcf47a635783ee58c43da15fd191cef7ec15262
fedt <dota2mm@163.com>
2017-04-05 23:50:19 +0800

    updata 4.7.1

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
   name: config.session.key, // 设置 session 中保存 session id 的字段名称
   secret: config.session.secret, // 通过 secret 来计算 hash 值并放在 cookie 中，使产生的 signedCookie 防篡改
+  resave: true, // 强制更新 session
+  saveUninitialized: false, // 强制创建一个 session ，即使用户未登录
   cookie: {
     maxAge: config.session.maxAge // 过期时间，过期后 cookie 中的 session id 自动删除
   },
@@ -38,13 +40,15 @@ app.use(require('express-formidable')({
   keepExtensions: true// 保留后缀
 }))
 
-// 设置模板全局常量
+// app.locals (程序层面) 和 res.locals (请求层面)是 express 提供的两个可用于模板渲染中的对象
+// 除这两个外，还有通过 app.set(key, value) 得到的 app.setting 也可以在模板中访问： setting.key
+// 设置模板全局常量，将常量挂载到app.locals
 app.locals.blog = {
   title: pkg.name,
   description: pkg.description
 }
 
-// 添加模板必需的三个变量
+// 添加模板必需的三个变量, 变量挂载到res.locals
 app.use(function (req, res, next) {
   res.locals.user = req.session.user
   res.locals.success = req.flash('success').toString()
